70e9b4d96ac2f497e78eecdbd78b3c5f0d33b364
FireInjun <Fireinjun291@gmail.com>
2018-06-09 02:07:12 -0400

    more stuff changed
    can't remember what but apparently I haven't pushed my most recent.

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require( 'express' );
-const bodyParser = require( 'body-parser' );
+// ! const bodyParser = require( 'body-parser' );
 const actions = require( './data/helpers/actionModel' );
 const projects = require( './data/helpers/projectModel' );
 const CORS = require( 'cors' );
@@ -8,8 +8,6 @@ const port = 8080
 const server = express();
 
 server.use( express.json() );
-server.use( bodyParser.json() );
-server.use( bodyParser.urlencoded( { extended: true } ) );
 server.use( CORS() );
 
 
